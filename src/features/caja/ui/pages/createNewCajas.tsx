0ed308a348ced37ee9d7338a16f471f6a1fcfcfb
AdrianGzn <233318@ids.upchiapas.edu.mx>
2025-03-17 01:02:35 -0600

    avance de tabla en caja

--- src/features/caja/ui/pages/createNewCajas.tsx
+++ src/features/caja/ui/pages/createNewCajas.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Dashboard from "../../../../shared/ui/pages/dashboard.component";
-
 import { SelectButton } from 'primereact/selectbutton';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
@@ -16,7 +15,7 @@ interface Product {
 }
 
 export default function CreateNewCajas() {
-  const [size, setSize] = useState<'small' | 'large' | 'normal' | undefined>('normal');
+  const [size, setSize] = useState<'small' | 'large' | 'normal' | undefined>('small');
   const [products, setProducts] = useState<Product[]>([]);
 
   const sizeOptions = [
@@ -30,16 +29,20 @@ export default function CreateNewCajas() {
       { code: 'P001', name: 'Product 1', category: 'Category 1', quantity: 10 },
       { code: 'P002', name: 'Product 2', category: 'Category 2', quantity: 20 },
       { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
+      { code: 'P001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P002', name: 'Product 2', category: 'Category 2', quantity: 20 },
       { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
+      { code: 'P001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P002', name: 'Product 2', category: 'Category 2', quantity: 20 },
       { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
+      { code: 'P001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P002', name: 'Product 2', category: 'Category 2', quantity: 20 },
       { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
+      { code: 'P001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P002', name: 'Product 2', category: 'Category 2', quantity: 20 },
       { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
-      { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
-      { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
-      { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
-      { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
-      { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
-      { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
+      { code: 'P001', name: 'Product 1', category: 'Category 1', quantity: 10 },
+      { code: 'P002', name: 'Product 2', category: 'Category 2', quantity: 20 },
       { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
     ]);
   }, []);
@@ -47,27 +50,32 @@ export default function CreateNewCajas() {
   return (
     <div className="w-full h-screen">
       <Dashboard>
-        <div className="w-full h-full flex flex-col">
-          <div className="w-[80%] h-[100px] m-5 flex flex-col items-center bg-white p-5">
-            <SelectButton
-              value={size}
-              onChange={(e) => setSize(e.value)}
-              options={sizeOptions}
-            />
-
-            <DataTable
-              value={products}
-              paginator
-              rows={5}
-              rowsPerPageOptions={[5, 10, 25, 50]}
-              size={size}
-              tableStyle={{ maxWidth: '50rem' }}
+        <div className="w-full h-full flex flex-col items-center">
+          <div className="w-[95%] h-[260px] m-5 flex flex-col items-center bg-white p-5 rounded-xl">
+            <div className="w-full mb-2 flex justify-between">
+                <p>Naranjas</p>
+                <SelectButton
+                    value={size}
+                    onChange={(e) => setSize(e.value)}
+                    options={sizeOptions}
+                />
+            </div>
+ 
+            <div className="w-full flex-grow overflow-hidden">
+              <DataTable
+                value={products}
+                paginator
+                rows={3}
+                rowsPerPageOptions={[3, 6, 9, 12]}
+                size={size}
+                style={{ width: '100%', maxHeight: '100%', overflowY: 'auto', fontSize: '11px' }} 
             >
-              <Column field="code" header="Code" />
-              <Column field="name" header="Name" />
-              <Column field="category" header="Category" />
-              <Column field="quantity" header="Quantity" />
-            </DataTable>
+                <Column field="code" header="Code" />
+                <Column field="name" header="Name" />
+                <Column field="category" header="Category" />
+                <Column field="quantity" header="Quantity" />
+              </DataTable>
+            </div>
           </div>
         </div>
       </Dashboard>
