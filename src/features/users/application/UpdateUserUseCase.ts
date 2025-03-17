deda8d77cba502fb58fc74031e3ab0c1cd319249
mdz48 <107087433+mdz48@users.noreply.github.com>
2025-03-17 15:03:11 -0600

    Usersmethods (#1)
    * ✨CRUD para users
    ✨Login conectado con la API
    
    * ✨Refactor login functionality to handle success and error responses, update LoginResponse interface, and improve error handling in LoginController
    
    * ✨Toast para inicio de sesion

---
+++ src/features/users/application/UpdateUserUseCase.ts
@@ -0,0 +1,10 @@
+import { User } from "../domain/user.entity";
+import { ApiUserRepository } from "../infrastructure/apiUser.repository";
+
+export class CreateUserUseCase {
+  constructor(private userRepository: ApiUserRepository) {}
+
+  async execute(user: User): Promise<void> {
+    await this.userRepository.Update(user);
+  }
+}
--- src/features/users/infrastructure/apiUser.repository.ts
+++ src/features/users/infrastructure/apiUser.repository.ts
@@ -4,10 +4,10 @@ import { LoginRequest } from "../domain/LoginRequest";
 import { LoginResponse } from "../domain/LoginResponse";
 
 export class ApiUserRepository implements IUserRepository {
-    private usersURL = `${import.meta.env.API_URL}/users`;
+    private usersURL = `http://localhost:8080/users`;
     private headers = {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
+        // "Access-Control-Allow-Origin": "*",
     };
 
     async Create(user : User): Promise<User> {
@@ -19,7 +19,6 @@ export class ApiUserRepository implements IUserRepository {
         if (!response.ok) throw new Error("Error al crear el usuario");
         return response.json();
     }
-// Pendiente
     async Login(login : LoginRequest): Promise<LoginResponse> {
         const response = await fetch(`${this.usersURL}/login`, {
             method: "POST",
@@ -28,5 +27,24 @@ export class ApiUserRepository implements IUserRepository {
         });
         if (!response.ok) throw new Error("Error al iniciar sesión");
         return response.json();
+    }
+
+    async Update(user : User): Promise<User> {
+        const response = await fetch(`${this.usersURL}/${user.id}`, {
+            method: "PUT",
+            body: JSON.stringify(user),
+            headers: this.headers,
+        });
+        if (!response.ok) throw new Error("Error al actualizar el usuario");
+        return response.json();
+    }
+
+    async Delete(id : string): Promise<boolean> {
+        const response = await fetch(`${this.usersURL}/${id}`, {
+            method: "DELETE",
+            headers: this.headers,
+        });
+        if (!response.ok) throw new Error("Error al eliminar el usuario");
+        return true;
     }
   }
